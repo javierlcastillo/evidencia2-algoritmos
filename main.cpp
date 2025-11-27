40d4bc505aaaa04c1ef75a12f08f9643a14f2207
Javier <jazer2228@gmail.com>
2025-11-26 19:09:36 -0600

    Terminado. Faltan tweaks

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 #define INF 999999  // Definir INF para usar en Floyd-Warshall
 
+// Definicion de informacion necesaria para datos ingresados
 struct Colonia{
     int id;
     string nombre;
@@ -21,28 +22,41 @@ struct Colonia{
     bool esCentral;
 };
 
+/*  --- Estructura de Grafo ---
+ * => indiceColonias = hash para encontrar el indice 
+ * => colonias = hash con datos de colonias
+ * => edges = conexiones
+ * 
+ */
 struct Graph {
     int n, m;
     unordered_map<string, Colonia> colonias;
-    unordered_map<string, int> indiceColonias;  
+    unordered_map<string, int> indiceColonias;
+    unordered_map<int, string> stringColonias;
+    vector<int> arregloCentrales;  
+    vector<int> arregloNoCentrales;
     vector<pair<int, pair<int, int>>> edges;
     vector<pair<int,int>> selectedEdges;
 
      // Variables para algoritmos
     vector<int> parent;              // Para Union-Find (Kruskal)
     vector<vector<int>> distMatrix;  // Para Floyd-Warshall
+    // Dentro de cada "celda" es el mejor path
+    vector<vector<int>> succesorMatrix; // Helper matrix para encontrar el path en Floyd-Warshall
 
     Graph(int _n, int _m){
         this-> n = _n;
         this-> m  = _m;
         parent.resize(_n);
         distMatrix.assign(_n, vector<int>(_n, INF));
+        succesorMatrix.assign(_n, vector<int>(_n, INF));
     }
-
-    // u = salida del arco
-	// v = llegada del arco
-	// w = costo del arco
-
+    
+    /* u = salida del arco
+	 * v = llegada del arco
+	 * w = costo del arco
+     * 
+     */ // Funcion para agregar una conexion al grafo
 	void addEdge(string u, string v, int w){ // -> u vetice1, v vertice2, w costo
         auto it_u = indiceColonias.find(u);
         auto it_v = indiceColonias.find(v);
@@ -58,6 +72,9 @@ struct Graph {
         int idx_v = it_v->second;
         distMatrix[idx_u][idx_v] = min(distMatrix[idx_u][idx_v], w);
         distMatrix[idx_v][idx_u] = min(distMatrix[idx_v][idx_u], w);
+        // Inicializacion de conexiones en la matriz helper del path
+        succesorMatrix[idx_v][idx_u] = idx_u;
+        succesorMatrix[idx_u][idx_v] = idx_v; 
     }
 
     /* --- Manejo de colonias en <unoredered_map> ---
@@ -72,6 +89,7 @@ struct Graph {
             Colonia temp;
             int id = colonias.size();
             indiceColonias[_nombre] = id;
+            stringColonias[id] = _nombre;
             temp.id = id;
             temp.x = _x;
             temp.y = _y;
@@ -102,9 +120,10 @@ struct Graph {
     
     // 1. KRUSKAL 
     void kruskal(vector<pair<string, string>>& cableadoNuevo){
-        cout << "-------------------" << endl;
-        cout << "1 - Cableado óptimo de nueva fibra." << endl;
-        
+        cout << "--------------------------------------------------------" << endl;
+        cout << "       1   Cableado óptimo de nueva fibra óptica" << endl;
+        cout << "--------------------------------------------------------" << endl;
+
         // Marcar aristas nuevas
         set<pair<int, int>> nuevas;
         for (auto& e : cableadoNuevo){
@@ -146,7 +165,7 @@ struct Graph {
             }
         }
         
-        cout << "Costo total: " << costoTotal << endl << endl;
+        cout << "   -Costo total del cableado: " << costoTotal << endl << endl;
     }
     
 // ============================================
@@ -167,13 +186,90 @@ struct Graph {
                 for (int j = 0; j < n; j++){   // j = destino
                     if (distMatrix[i][k] != INF && distMatrix[k][j] != INF){
                         // ¿Es mejor ir i->k->j que i->j directo?
-                        distMatrix[i][j] = min(distMatrix[i][j], 
-                                              distMatrix[i][k] + distMatrix[k][j]);
+                        distMatrix[i][j] = min(distMatrix[i][j], distMatrix[i][k] + distMatrix[k][j]);
+                        succesorMatrix[i][j] = succesorMatrix[i][k];
+
                     }
                 }
             }
         }
     } 
+
+    void definirCentrales(){
+        // Encontrams que son y que no centrales
+        for (const auto& it : colonias){
+            auto encontrar = indiceColonias.find(it.first);
+            int indice =  encontrar->second;
+            if (it.second.esCentral) {
+                arregloCentrales.push_back(indice);
+            }
+            else{
+                arregloNoCentrales.push_back(indice);
+            }
+        }
+    }
+
+    void displayPath(int origen, int destino){
+        /*
+        La idea es utilizar successorMatrix para poder desplegar el path
+        */
+       int actual = origen;
+       if (distMatrix[origen][destino] == INF){
+            cout << "No hay ruta";
+            return;
+       } 
+       cout << actual << " -> "; 
+        while(actual != origen){
+            cout << actual;
+            actual = succesorMatrix[actual][destino];
+            if (actual != origen){
+                cout << " -> "; 
+            }
+        }
+    }
+
+    void shortestPath(){
+        int iteraciones = 0;
+        int costo_total = 0;
+        // se definen que tipo de colonia se va a utilizar
+        const vector<int> &visitarColonias = arregloNoCentrales;
+        cout << "--------------------------------------------------------" << endl;
+        cout << " 2  Ruta más corta para visitar cada colonia NO central" << endl;
+        cout << "--------------------------------------------------------" << endl;
+        vector<int> ruta;
+        unordered_map<int, bool> visitadas;
+        int inicio = 0;
+        int actual = inicio; 
+        ruta.push_back(actual);
+        visitadas[actual] = true; 
+       for(int i = 0; i < visitarColonias.size()-1;     i++){
+            int coloniaCercana = -1; // cual es la colonia mas cercana a visitar en indice
+            int distMinima = INF;   // cual es su distancia en "costo"
+            for(int it_colonia : visitarColonias){
+                // Ya visitamos esta colonia?
+                if(visitadas.find(it_colonia) == visitadas.end()){
+                    if(distMatrix[actual][it_colonia] < distMinima){
+                        distMinima = distMatrix[actual][it_colonia]; 
+                        coloniaCercana = it_colonia; 
+                    }
+                }
+                iteraciones++;
+                cout << iteraciones << endl;
+            }
+            if (coloniaCercana != -1){
+                costo_total += distMinima;
+                actual = coloniaCercana;
+                ruta.push_back(actual);
+                visitadas[actual] = true;
+            }
+        }
+        costo_total += distMatrix[actual][inicio];
+        actual = inicio;
+        while (actual + 1 < ruta.size()){
+            displayPath(actual, actual + 1); 
+        }
+        
+    }
 
      // ============================================
     // ALGORITMO 4: DISTANCIA EUCLIDIANA
@@ -209,30 +305,28 @@ struct Graph {
              << ") -> (" << x_cercana << ", " << y_cercana << ")" << endl;
     }
 
+    
+
 };
 
 int main (){
-
+    // Lectura de valores
     int n, m, k, q;
     cin >> n >> m >> k >> q;
-
     Graph g(n, m);
-
     int x, y, central, costo; 
     string nombre1, nombre2;
 
+    // LLenar la información de las colonias.
     for(int i = 0; i < n; i++){
-
         cin >> nombre1 >> x >> y >> central;
         g.addColoniaInMap(nombre1, x, y, central);
-
     }
 
+    // Agregar las conexiones entre colonias
     for(int i = 0; i < m; i++){
-
         cin >> nombre1 >> nombre2 >> costo;
         g.addEdge(nombre1, nombre2, costo);
-
     }
 
      // Leer k conexiones de cableado nuevo
@@ -241,9 +335,21 @@ int main (){
         cin >> nombre1 >> nombre2;
         cableadoNuevo.push_back({nombre1, nombre2});
     }
-    
-    // Ejecutar Kruskal para encontrar MST
-    g.kruskal(cableadoNuevo);
 
+    /* --- PROBLEMA 1 --- 
+     * Se despliega la forma óptima de cablear la nueva fibra óptica compartiendo
+     * informción de el menor costo posible. 
+     * Se utiliza Kruskal
+     */
+    g.kruskal(cableadoNuevo);
+    /* --- PROBLEMA 1 --- 
+     * Se busca encontrar una forma eficiente de visitar todas las colonias que NO SON CENTRALES
+     * El algoritmo requiere de la incializacion de FloydWarshall para conocer los costos minimos y
+     * conocer cuales son las rutas para estos caminos minimos.
+     * Se llama definir centrales ya que es el que envia el arreglo con las centrales a recorrer
+     */
+    g.floydWarshall();
+    g.definirCentrales();
+    g.shortestPath();
     return 0;
 }
